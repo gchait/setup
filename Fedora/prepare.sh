2f7da8cd8c8b6b7ccc2836216c1cc54df22eb3e6
Guy Chait <53366531+gchait@users.noreply.github.com>
2024-07-04 10:31:53 +0300

    x

--- Fedora/prepare.sh
+++ Fedora/prepare.sh
@@ -6,7 +6,7 @@ sudo dnf install -y \
     tree vim python3-pip jq yq awscli2 iproute iputils \
     kubernetes-client just eza cronie figlet nc htop zsh \
     asciinema lolcat zip gzip tar curl wget dnsutils sudo \
-    ncurses dnf-plugins-core dnf-utils git findutils
+    fastfetch ncurses dnf-plugins-core dnf-utils git findutils
 
 sudo chsh -s $(which zsh) "${USER}"
 mkdir -p "${HOME}/.zsh"
@@ -19,7 +19,8 @@ get_repo "${HOME}/.zsh/complete" https://github.com/zsh-users/zsh-completions.gi
 get_repo "${HOME}/.zsh/highlight" https://github.com/zsh-users/zsh-syntax-highlighting.git
 get_repo "${HOME}/.zsh/suggest" https://github.com/zsh-users/zsh-autosuggestions.git
 get_repo "${HOME}/.zsh/p10k" https://github.com/romkatv/powerlevel10k.git
-
 get_repo "${HOME}/setup" https://github.com/gchait/setup.git
+
 sudo cp "${HOME}/setup/Fedora/dnf.conf" /etc/dnf/
 cp -r ${HOME}/setup/Fedora/Home/.* "${HOME}"
+mkdir -p "${HOME}/Projects"
