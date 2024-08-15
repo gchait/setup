3c58716e8d9e989ee0eb426861d12111110e403d
Guy Chait <53366531+gchait@users.noreply.github.com>
2024-08-15 18:06:24 +0300

    Update prepare.ps1

--- Windows/prepare.ps1
+++ Windows/prepare.ps1
@@ -10,8 +10,10 @@ reg import "${HOME}\scoop\apps\vscode\current\install-context.reg" *> ${null}
 reg import "${HOME}\scoop\apps\vscode\current\install-associations.reg" *> ${null}
 
 code `
-    --install-extension ms-vscode-remote.remote-wsl `
     --install-extension JonathanHarty.gruvbox-material-icon-theme `
+    --install-extension antfu.icons-carbon `
+    --install-extension sveggiani.vscode-field-lights `
+    --install-extension ms-vscode-remote.remote-wsl `
     --install-extension eamodio.gitlens `
     --install-extension ecmel.vscode-html-css `
     --install-extension esbenp.prettier-vscode `
@@ -20,14 +22,9 @@ code `
     --install-extension ms-azuretools.vscode-docker `
     --install-extension ms-kubernetes-tools.vscode-kubernetes-tools `
     --install-extension ms-pyright.pyright `
-    --install-extension antfu.icons-carbon `
     --install-extension redhat.vscode-yaml `
     --install-extension samuelcolvin.jinjahtml `
     --install-extension tamasfe.even-better-toml `
-    --install-extension sveggiani.vscode-field-lights `
-    --install-extension vscjava.vscode-java-pack `
-    --install-extension richardwillis.vscode-gradle-extension-pack `
-    --install-extension vmware.vscode-boot-dev-pack `
     --install-extension bierner.markdown-mermaid `
     | Select-String -NotMatch -Pattern "already installed" | % { $_.Line }
 
