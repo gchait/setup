2c1892b76e2a7a710db9eabb1db9157396836fd5
Guy Chait <53366531+gchait@users.noreply.github.com>
2024-08-23 17:01:27 +0300

    x

--- Windows/prepare.ps1
+++ Windows/prepare.ps1
@@ -12,7 +12,7 @@ reg import "${HOME}\scoop\apps\vscode\current\install-associations.reg" *> ${nul
 code `
     --install-extension JonathanHarty.gruvbox-material-icon-theme `
     --install-extension antfu.icons-carbon `
-    --install-extension sveggiani.vscode-field-lights `
+    --install-extension ionutvmi.spacegray-vscode `
     --install-extension ms-vscode-remote.remote-wsl `
     --install-extension eamodio.gitlens `
     --install-extension ecmel.vscode-html-css `
