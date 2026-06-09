d2a82229956bdfcfa842c44d5fe0c7f0334521a8
Guy Chait <53366531+gchait@users.noreply.github.com>
2026-06-09 22:35:13 +0300

    move kubectl to versioned

--- Fedora/distro.sh
+++ Fedora/distro.sh
@@ -5,14 +5,15 @@ DNF_PKGS=(
   clean-rpm-gpg-pubkey cmatrix containerd.io cups-libs dnsutils docker-buildx-plugin
   docker-ce docker-ce-cli docker-compose-plugin eza fastfetch fd-find figlet findutils
   gdk-pixbuf2-devel golang gron gtk3 gtk3-devel gzip helm htop hugo iproute iptables-legacy
-  iptables-utils iputils jq just kubernetes1.34-client libXScrnSaver libXScrnSaver-devel
-  libXcomposite libXcursor libXdamage libXext libXi libXrandr libXtst lolcat make maven
-  mesa-libgbm moreutils-parallel ncurses nmap-ncat nss-devel openssl packer pango
-  python-unversioned-command python3-pip qemu-user-static remove-retired-packages ripgrep
-  rpmconf shfmt symlinks tar terraform tokei tree vim wget xrandr yq zip zsh
+  iptables-utils iputils jq just libXScrnSaver libXScrnSaver-devel libXcomposite libXcursor
+  libXdamage libXext libXi libXrandr libXtst lolcat make maven mesa-libgbm moreutils-parallel
+  ncurses nmap-ncat nss-devel openssl packer pango python-unversioned-command python3-pip
+  qemu-user-static remove-retired-packages ripgrep rpmconf shfmt symlinks tar terraform tokei
+  tree vim wget xrandr yq zip zsh
 )
 
 ALT_PY_VER="3.9"
+KUBECTL_VER="1.34"
 # shellcheck disable=SC2034
 DISTRO_NAME="Fedora"
 
@@ -36,12 +37,13 @@ packages_setup() {
   local java="java-${JAVA_VER}-openjdk-devel"
   local alt_java="java-${ALT_JAVA_VER}-openjdk-devel"
   local alt_py="python${ALT_PY_VER}"
+  local kubectl="kubernetes${KUBECTL_VER}-client"
 
   local arch
   arch=$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')
 
   sudo dnf install -yq \
-    "${java}" "${alt_java}" "${alt_py}" \
+    "${java}" "${alt_java}" "${alt_py}" "${kubectl}" \
     "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_${arch/amd64/64bit}/session-manager-plugin.rpm" \
     "https://github.com/lucagrulla/cw/releases/latest/download/cw_${arch}.rpm" \
     "${DNF_PKGS[@]}" 2> /dev/null
