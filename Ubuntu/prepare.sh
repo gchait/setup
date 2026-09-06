73f48bbfa1588a8f89b8cda09366325e89565e4a
Guy Chait <53366531+gchait@users.noreply.github.com>
2026-09-06 22:42:37 +0300

    zsh-prefix-history

--- Ubuntu/prepare.sh
+++ Ubuntu/prepare.sh
@@ -90,7 +90,7 @@ BOOTSTRAP_APT_PKGS="ca-certificates curl git gnupg"
 
 APT_PKGS="
   adwaita-icon-theme asciinema bat bind9-dnsutils build-essential cmatrix docker-buildx
-  docker-compose-v2 docker.io eza fd-find figlet glab golang-go gron helm htop hugo iproute2
+  docker-compose-v2 docker.io eza fd-find figlet gh glab golang-go gron helm htop hugo iproute2
   iptables jq just kubectl libasound2-dev libasound2t64 libatk1.0-0t64 libcups2t64 libgbm1
   libgdk-pixbuf-2.0-dev libgtk-3-0t64 libgtk-3-dev libncurses6 libnss3-dev libpango-1.0-0
   libxcomposite1 libxcursor1 libxdamage1 libxext6 libxi6 libxrandr2 libxss-dev libxss1 libxtst6
